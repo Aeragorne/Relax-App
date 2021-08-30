50c8b728c08b84f316db18c3ab44ce91bd455357
Carlos De Cabrera <Decabreracarlos@gmail.com>
2021-08-30 17:35:37 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+const totalTime = 7500
+const breatheTime = (totalTime/5) * 2
+const holdtTime = totalTime / 5
+
+
+function breatheAnimation () {
+  text.innerHTML = 'Breath in'
+  container.className = 'container grow'
+
+  setTimeout(() => {
+    text.innerText = 'Hold'
+
+    setTimeout(() => {
+      text.innerText = 'Breath Out!'
+      container.className = 'container shrink'
+    }, holdTime)
+  },breatheTime)
+}
+
+setInterval(breatheAnimation, totalTime )
